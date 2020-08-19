eeab508fb16458cd760b4b11cef057c79053bda8
Andreis3 <andrei.as3@hotmail.com>
2020-08-19 18:48:37 -0300

    chore: add editorconfig and prettier and jest.config.unit

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+  "standard-with-typescript",
+  "plugin:@typescript-eslint/recommended",
+  "prettier/@typescript-eslint",
+  "plugin:prettier/recommended"
+],
+plugins: ['prettier'],
+globals: {
+  Atomics: 'readonly',
+  SharedArrayBuffer: 'readonly',
+},
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: "module",
+    project: "./tsconfig.json",
+    ecmaFeatures: {
+      jsx: true // Allows for the parsing of JSX
+    }
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-new": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    //"@typescript-eslint/explicit-function-return-type": "off"
+  },
+}
