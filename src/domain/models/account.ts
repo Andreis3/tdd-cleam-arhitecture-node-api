25ec4268091d9d0ffa65140a6d7d6f3d2533c1fa
Andreis3 <andrei.as3@hotmail.com>
2020-09-14 06:33:22 -0300

    feat: ensure AccountMongoRepository returns an account on sucess

--- src/domain/models/account.ts
+++ src/domain/models/account.ts
@@ -1,5 +1,6 @@
 export interface IAccountModel {
     id: string;
     name: string;
+    email: string;
     password: string;
 }
---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,14 @@
+import { IAddAccountRepository } from '../../../../data/protocols/IAddAccountRepository';
+import { IAddAccountModel } from '../../../../domain/use-cases/IAddAccount';
+import { IAccountModel } from '../../../../domain/models/account';
+import { MongoHelper } from '../helpers/mongoHelpers';
+
+export class AccountMongoRepository implements IAddAccountRepository {
+    async add(accountData: IAddAccountModel): Promise<IAccountModel> {
+        const accountCollection = await MongoHelper.getCollection('accounts');
+        const result = await accountCollection.insertOne(accountData);
+        const account = result.ops[0];
+        const { _id, ...accountWithoutId } = account;
+        return Object.assign({}, accountWithoutId, { id: _id });
+    }
+}
