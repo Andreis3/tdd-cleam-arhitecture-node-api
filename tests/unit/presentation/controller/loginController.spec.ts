4684aaf3ae7c57554b4d35b47bd620b7da2e118c
Andreis3 <andrei.as3@hotmail.com>
2020-12-01 07:27:49 -0300

    test: ensure LoginController calls EmailValidator with correct email

--- tests/unit/presentation/controller/loginController.spec.ts
+++ tests/unit/presentation/controller/loginController.spec.ts
@@ -1,14 +1,28 @@
 import { LoginController } from '../../../../src/presentation/controllers/login/loginController';
+import { IEmailValidator } from '../../../../src/presentation/controllers/signup/signupProtocols';
 import { MissingParamError } from '../../../../src/presentation/errors';
 import { badRequest } from '../../../../src/presentation/helpers/httpHelpers';
 
+const makeEmailValidator = (): IEmailValidator => {
+    class EmailValidatorStub implements IEmailValidator {
+        isValid(email: string): boolean {
+            return true;
+        }
+    }
+    return new EmailValidatorStub();
+};
 interface ISutTypes {
     sut: LoginController;
+    emailValidatorStub: IEmailValidator;
 }
 
 const makeSut = (): ISutTypes => {
-    const sut = new LoginController();
-    return { sut };
+    const emailValidatorStub = makeEmailValidator();
+    const sut = new LoginController(emailValidatorStub);
+    return {
+        sut,
+        emailValidatorStub,
+    };
 };
 describe('login Controller', () => {
     test('Should return 400 if no email is provider', async () => {
@@ -33,5 +47,20 @@ describe('login Controller', () => {
         const httpResponse = await sut.handle(httpRequest);
 
         expect(httpResponse).toEqual(badRequest(new MissingParamError('password')));
+    });
+
+    test('Should call EmailValidator with correct email', async () => {
+        const { sut, emailValidatorStub } = makeSut();
+        const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid');
+
+        const httpRequest = {
+            body: {
+                email: 'any_email@mail.com',
+                password: 'any_password',
+            },
+        };
+        await sut.handle(httpRequest);
+
+        expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com');
     });
 });
