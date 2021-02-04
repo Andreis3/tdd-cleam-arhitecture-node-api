b8558715e00e6e081debddf809fa9ecd58b32110
Andreis3 <andrei.as3@hotmail.com>
2021-02-04 00:04:01 -0300

    test: refactor test

--- tests/unit/presentation/helpers/validators/ValidationComposite.spec.ts
+++ tests/unit/presentation/helpers/validators/ValidationComposite.spec.ts
@@ -6,13 +6,13 @@ describe('Validation Composite ', () => {
     test('Should return an error if any validation fails', () => {
         class ValidationStub implements IValidation {
             validate(input: any): Error {
-                return new MissingParamError('filed');
+                return new MissingParamError('field');
             }
         }
 
         const validationStub = new ValidationStub();
         const sut = new ValidationComposite([validationStub]);
         const error = sut.validate({ field: 'any_value' });
-        expect(error).toEqual(new MissingParamError('filed'));
+        expect(error).toEqual(new MissingParamError('field'));
     });
 });
