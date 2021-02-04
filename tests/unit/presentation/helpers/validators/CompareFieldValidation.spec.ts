e9b430c3eecead27477613e243d1f14547151904
Andreis3 <andrei.as3@hotmail.com>
2021-02-03 21:59:15 -0300

    test: add tests to CompareFieldsValidation

---
+++ tests/unit/presentation/helpers/validators/CompareFieldValidation.spec.ts
@@ -0,0 +1,25 @@
+import { InvalidParamError } from '../../../../../src/presentation/errors';
+import { CompareFieldsValidation } from '../../../../../src/presentation/helpers/validators/CompareFieldValidation';
+
+const makeSut = (): CompareFieldsValidation => {
+    return new CompareFieldsValidation('field', 'filedToCompare');
+};
+describe('CompareFields Validation', () => {
+    test('Should return a MissingParamError if validation fails', () => {
+        const sut = makeSut();
+        const error = sut.validate({
+            field: 'any_value',
+            filedToCompare: 'wrong_value',
+        });
+        expect(error).toEqual(new InvalidParamError('filedToCompare'));
+    });
+
+    test('Should not return if validation succeeds', () => {
+        const sut = makeSut();
+        const error = sut.validate({
+            field: 'any_value',
+            filedToCompare: 'any_value',
+        });
+        expect(error).toBeFalsy();
+    });
+});
--- tests/unit/presentation/helpers/validators/RequiredFieldValidation.spec.ts
+++ tests/unit/presentation/helpers/validators/RequiredFieldValidation.spec.ts
@@ -1,15 +1,18 @@
 import { MissingParamError } from '../../../../../src/presentation/errors';
 import { RequiredFieldValidation } from '../../../../../src/presentation/helpers/validators/RequiredFieldValidation';
 
+const makeSut = (): RequiredFieldValidation => {
+    return new RequiredFieldValidation('field');
+};
 describe('RequiredField Validation', () => {
     test('Should return a MissingParamError if validation fails', () => {
-        const sut = new RequiredFieldValidation('field');
+        const sut = makeSut();
         const error = sut.validate({ name: 'any_name' });
         expect(error).toEqual(new MissingParamError('field'));
     });
 
     test('Should not return if validation succeeds', () => {
-        const sut = new RequiredFieldValidation('field');
+        const sut = makeSut();
         const error = sut.validate({ field: 'any_name' });
         expect(error).toBeFalsy();
     });
