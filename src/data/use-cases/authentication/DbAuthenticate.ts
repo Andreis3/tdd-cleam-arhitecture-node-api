8e6a508bad8370d0cfad3bb1742fdb7aab71ffef
Andreis3 <andrei.as3@hotmail.com>
2021-02-09 21:52:29 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/use-cases/authentication/DbAuthenticate.ts
+++ src/data/use-cases/authentication/DbAuthenticate.ts
@@ -2,20 +2,24 @@ import { IAuthentication, IAuthenticationModel } from '../../../domain/use-cases
 import { IHashComparer } from '../../protocols/cryptography/IHashComparer';
 import { ITokenGenerator } from '../../protocols/cryptography/ITokenGenerator';
 import { ILoadAccountByEmailRepository } from '../../protocols/db/ILoadAccountByEmailRepository';
+import { IUpdateAccessTokenRepository } from '../../protocols/db/IUpdateAccessTokenRepository';
 
 export class DbAuthentication implements IAuthentication {
     private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository;
     private readonly hashComparer: IHashComparer;
     private readonly tokenGenerator: ITokenGenerator;
+    private readonly updateAccessTokenRepository: IUpdateAccessTokenRepository;
 
     constructor(
         loadAccountByEmailRepository: ILoadAccountByEmailRepository,
         hashComparer: IHashComparer,
         tokenGenerator: ITokenGenerator,
+        updateAccessTokenRepository: IUpdateAccessTokenRepository,
     ) {
         this.loadAccountByEmailRepository = loadAccountByEmailRepository;
         this.hashComparer = hashComparer;
         this.tokenGenerator = tokenGenerator;
+        this.updateAccessTokenRepository = updateAccessTokenRepository;
     }
 
     async auth(authentication: IAuthenticationModel): Promise<string> {
@@ -24,6 +28,7 @@ export class DbAuthentication implements IAuthentication {
             const isValid = await this.hashComparer.compare(authentication.password, account.password);
             if (isValid) {
                 const accessToken = await this.tokenGenerator.generate(account.id);
+                await this.updateAccessTokenRepository.update(account.id, accessToken);
                 return accessToken;
             }
         }
