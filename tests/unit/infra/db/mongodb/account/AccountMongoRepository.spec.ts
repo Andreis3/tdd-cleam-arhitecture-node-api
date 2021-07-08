f95946fa496622b3ce47ba84e4c2e3798d239a8a
Andreis3 <andrei.as3@hotmail.com>
2021-07-08 05:51:08 -0300

    refactor: separate test in describ blocks

--- tests/unit/infra/db/mongodb/account/AccountMongoRepository.spec.ts
+++ tests/unit/infra/db/mongodb/account/AccountMongoRepository.spec.ts
@@ -15,61 +15,66 @@ describe('account Mongo Repository', () => {
         accountCollection = await MongoHelper.getCollection('accounts');
         await accountCollection.deleteMany({});
     });
+    describe('aadd()', () => {
+        test('Should return an account on add success', async () => {
+            const sut = new AccountMongoRepository();
+            const account = await sut.add({
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                password: 'any_password',
+            });
 
-    test('Should return an account on add success', async () => {
-        const sut = new AccountMongoRepository();
-        const account = await sut.add({
-            name: 'any_name',
-            email: 'any_email@mail.com',
-            password: 'any_password',
+            expect(account).toBeTruthy();
+            expect(account.id).toBeTruthy();
+            expect(account.name).toBe('any_name');
+            expect(account.email).toBe('any_email@mail.com');
+            expect(account.password).toBe('any_password');
         });
-
-        expect(account).toBeTruthy();
-        expect(account.id).toBeTruthy();
-        expect(account.name).toBe('any_name');
-        expect(account.email).toBe('any_email@mail.com');
-        expect(account.password).toBe('any_password');
     });
 
-    test('Should return an account on loadByEmail success', async () => {
-        const sut = new AccountMongoRepository();
-        await accountCollection.insertOne({
-            name: 'any_name',
-            email: 'any_email@mail.com',
-            password: 'any_password',
-        });
-        const account = await sut.loadByEmail('any_email@mail.com');
+    describe('loadByEmail()', () => {
+        test('Should return an account on loadByEmail success', async () => {
+            const sut = new AccountMongoRepository();
+            await accountCollection.insertOne({
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                password: 'any_password',
+            });
+            const account = await sut.loadByEmail('any_email@mail.com');
 
-        expect(account).toBeTruthy();
-        expect(account.id).toBeTruthy();
-        expect(account.name).toBe('any_name');
-        expect(account.email).toBe('any_email@mail.com');
-        expect(account.password).toBe('any_password');
-    });
-
-    test('Should return null if loadByEmail fails', async () => {
-        const sut = new AccountMongoRepository();
-
-        const account = await sut.loadByEmail('any_email@mail.com');
-
-        expect(account).toBeFalsy();
-    });
-
-    test('Should update the account on updateAccessToken success', async () => {
-        const sut = new AccountMongoRepository();
-        const res = await accountCollection.insertOne({
-            name: 'any_name',
-            email: 'any_email@mail.com',
-            password: 'any_password',
+            expect(account).toBeTruthy();
+            expect(account.id).toBeTruthy();
+            expect(account.name).toBe('any_name');
+            expect(account.email).toBe('any_email@mail.com');
+            expect(account.password).toBe('any_password');
         });
 
-        expect(res.ops[0].accessToken).toBeFalsy();
+        test('Should return null if loadByEmail fails', async () => {
+            const sut = new AccountMongoRepository();
 
-        await sut.updateAccessToken(res.ops[0]._id, 'any_token');
+            const account = await sut.loadByEmail('any_email@mail.com');
 
-        const account = await accountCollection.findOne({ _id: res.ops[0]._id });
+            expect(account).toBeFalsy();
+        });
+    });
 
-        expect(account).toBeTruthy();
-        expect(account.accessToken).toBe('any_token');
+    describe('updateAccessToken()', () => {
+        test('Should update the account on updateAccessToken success', async () => {
+            const sut = new AccountMongoRepository();
+            const res = await accountCollection.insertOne({
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                password: 'any_password',
+            });
+
+            expect(res.ops[0].accessToken).toBeFalsy();
+
+            await sut.updateAccessToken(res.ops[0]._id, 'any_token');
+
+            const account = await accountCollection.findOne({ _id: res.ops[0]._id });
+
+            expect(account).toBeTruthy();
+            expect(account.accessToken).toBe('any_token');
+        });
     });
 });
