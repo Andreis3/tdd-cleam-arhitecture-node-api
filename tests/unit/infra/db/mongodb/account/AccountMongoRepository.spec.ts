abaf1f40ee6449453bee18bcf60cdc32f99704ac
Andreis3 <andrei.as3@hotmail.com>
2021-07-08 22:43:38 -0300

    test: ensure AccountMongoRepository find an account by token with role

--- tests/unit/infra/db/mongodb/account/AccountMongoRepository.spec.ts
+++ tests/unit/infra/db/mongodb/account/AccountMongoRepository.spec.ts
@@ -79,7 +79,7 @@ describe('account Mongo Repository', () => {
     });
 
     describe('loadByToken()', () => {
-        test('Should return an account on loadByToken success', async () => {
+        test('Should return an account on loadByToken without role', async () => {
             const sut = new AccountMongoRepository();
             await accountCollection.insertOne({
                 name: 'any_name',
@@ -96,12 +96,22 @@ describe('account Mongo Repository', () => {
             expect(account.password).toBe('any_password');
         });
 
-        test('Should return null if loadByEmail fails', async () => {
+        test('Should return an account on loadByToken with role', async () => {
             const sut = new AccountMongoRepository();
+            await accountCollection.insertOne({
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                password: 'any_password',
+                accessToken: 'any_token',
+                role: 'any_role',
+            });
+            const account = await sut.loadByToken('any_token', 'any_role');
 
-            const account = await sut.loadByEmail('any_email@mail.com');
-
-            expect(account).toBeFalsy();
+            expect(account).toBeTruthy();
+            expect(account.id).toBeTruthy();
+            expect(account.name).toBe('any_name');
+            expect(account.email).toBe('any_email@mail.com');
+            expect(account.password).toBe('any_password');
         });
     });
 });
