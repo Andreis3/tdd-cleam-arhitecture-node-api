652c18012ddcb4a0603884eebf07a448a83f39b6
Andreis3 <andrei.as3@hotmail.com>
2021-07-08 22:40:41 -0300

    test: ensure AccountMongoRepository find an account by token without role

--- tests/unit/infra/db/mongodb/account/AccountMongoRepository.spec.ts
+++ tests/unit/infra/db/mongodb/account/AccountMongoRepository.spec.ts
@@ -77,4 +77,31 @@ describe('account Mongo Repository', () => {
             expect(account.accessToken).toBe('any_token');
         });
     });
+
+    describe('loadByToken()', () => {
+        test('Should return an account on loadByToken success', async () => {
+            const sut = new AccountMongoRepository();
+            await accountCollection.insertOne({
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                password: 'any_password',
+                accessToken: 'any_token',
+            });
+            const account = await sut.loadByToken('any_token');
+
+            expect(account).toBeTruthy();
+            expect(account.id).toBeTruthy();
+            expect(account.name).toBe('any_name');
+            expect(account.email).toBe('any_email@mail.com');
+            expect(account.password).toBe('any_password');
+        });
+
+        test('Should return null if loadByEmail fails', async () => {
+            const sut = new AccountMongoRepository();
+
+            const account = await sut.loadByEmail('any_email@mail.com');
+
+            expect(account).toBeFalsy();
+        });
+    });
 });
