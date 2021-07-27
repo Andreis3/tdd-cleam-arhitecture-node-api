da12f669de1eb668cca2a51c61d5e3e693799998
Andrei Santos <andrei.as3@hotmail.com>
2021-07-26 21:34:39 -0300

    refactor: organize describe test

--- tests/unit/infra/db/mongodb/survey/SurveyMongoRepository.spec.ts
+++ tests/unit/infra/db/mongodb/survey/SurveyMongoRepository.spec.ts
@@ -23,24 +23,25 @@ describe('Survey Mongo Repository', () => {
     const makeSut = (): SurveyMongoRepository => {
         return new SurveyMongoRepository();
     };
+    describe('add', () => {
+        test('Should add a survey on add success', async () => {
+            const sut = makeSut();
+            await sut.add({
+                question: 'any_question',
+                answers: [
+                    {
+                        image: 'any_image',
+                        answer: 'any_answer',
+                    },
+                    {
+                        answer: 'other_answer',
+                    },
+                ],
+                date: new Date(),
+            });
 
-    test('Should add a survey on add success', async () => {
-        const sut = makeSut();
-        await sut.add({
-            question: 'any_question',
-            answers: [
-                {
-                    image: 'any_image',
-                    answer: 'any_answer',
-                },
-                {
-                    answer: 'other_answer',
-                },
-            ],
-            date: new Date(),
+            const survey = await surveyCollection.findOne({ question: 'any_question' });
+            expect(survey).toBeTruthy();
         });
-
-        const survey = await surveyCollection.findOne({ question: 'any_question' });
-        expect(survey).toBeTruthy();
     });
 });
