bbe7b70274bcaedfbab1d5824746204e34033539
Andrei Santos <andrei.as3@hotmail.com>
2021-07-26 21:48:08 -0300

    test: load all surveys

--- tests/unit/infra/db/mongodb/survey/SurveyMongoRepository.spec.ts
+++ tests/unit/infra/db/mongodb/survey/SurveyMongoRepository.spec.ts
@@ -23,6 +23,7 @@ describe('Survey Mongo Repository', () => {
     const makeSut = (): SurveyMongoRepository => {
         return new SurveyMongoRepository();
     };
+
     describe('add', () => {
         test('Should add a survey on add success', async () => {
             const sut = makeSut();
@@ -42,6 +43,45 @@ describe('Survey Mongo Repository', () => {
 
             const survey = await surveyCollection.findOne({ question: 'any_question' });
             expect(survey).toBeTruthy();
+        });
+    });
+
+    describe('loadAll', () => {
+        test('Should loadAll a surveys on add success', async () => {
+            await surveyCollection.insertMany([
+                {
+                    question: 'any_question',
+                    answers: [
+                        {
+                            image: 'any_image',
+                            answer: 'any_answer',
+                        },
+                        {
+                            answer: 'other_answer',
+                        },
+                    ],
+                    date: new Date(),
+                },
+                {
+                    question: 'any_question_2',
+                    answers: [
+                        {
+                            image: 'any_image_2',
+                            answer: 'any_answer_2',
+                        },
+                        {
+                            answer: 'other_answer_2',
+                        },
+                    ],
+                    date: new Date(),
+                },
+            ]);
+            const sut = makeSut();
+            const surveys = await sut.loadAll();
+
+            expect(surveys.length).toBe(2);
+            expect(surveys[0].question).toBe('any_question');
+            expect(surveys[1].question).toBe('any_question_2');
         });
     });
 });
