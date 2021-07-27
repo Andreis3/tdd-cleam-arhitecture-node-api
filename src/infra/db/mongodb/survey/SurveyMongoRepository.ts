4c3d726020bce04e8e3d36213c873f00d8c157b7
Andrei Santos <andrei.as3@hotmail.com>
2021-07-26 21:47:53 -0300

    feat: load all surveys

--- src/infra/db/mongodb/survey/SurveyMongoRepository.ts
+++ src/infra/db/mongodb/survey/SurveyMongoRepository.ts
@@ -1,9 +1,17 @@
+import { ILoadSurveysRepository } from '../../../../data/protocols/db/survey/ILoadSurveysRepository';
 import { IAddSurveyModel, IAddSurveyRepository } from '../../../../data/use-cases/add-survey/DbAddSurveyProtocols';
+import { ISurveyModel } from '../../../../domain/models/ISurveyModel';
 import { MongoHelper } from '../helpers/MongoHelpers';
 
-export class SurveyMongoRepository implements IAddSurveyRepository {
+export class SurveyMongoRepository implements IAddSurveyRepository, ILoadSurveysRepository {
     async add(surveyData: IAddSurveyModel): Promise<void> {
         const surveyCollection = await MongoHelper.getCollection('survey');
         await surveyCollection.insertOne(surveyData);
+    }
+
+    async loadAll(): Promise<ISurveyModel[]> {
+        const surveyCollection = await MongoHelper.getCollection('survey');
+        const surveys: ISurveyModel[] = await surveyCollection.find().toArray();
+        return surveys;
     }
 }
