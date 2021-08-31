4634e1f068782e55aaf8fb6e12b0830818f74f9e
Andrei Santos <andrei.as3@hotmail.com>
2021-08-31 07:40:40 -0300

    refactor: cretae make access token

--- tests/integration/main/routes/SurveyRoutes.spec.ts
+++ tests/integration/main/routes/SurveyRoutes.spec.ts
@@ -8,6 +8,28 @@ import env from '../../../../src/main/config/env';
 let surveyCollection: Collection;
 let accountCollection: Collection;
 
+const makeAccessToken = async (): Promise<string> => {
+    const res = await accountCollection.insertOne({
+        name: 'Andrei',
+        email: 'andrei.santos@test.com',
+        password: '123',
+        role: 'admin',
+    });
+
+    const id = res.ops[0]._id;
+    const accessToken = sign({ id }, env.jwtSecret);
+    await accountCollection.updateOne(
+        {
+            _id: id,
+        },
+        {
+            $set: { accessToken },
+        },
+    );
+
+    return accessToken;
+};
+
 beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
 });
@@ -43,23 +65,7 @@ describe('Main :: Routes :: SurveyRoutes', () => {
         });
 
         test('Should return 204 on add survey with valid accessToken', async () => {
-            const res = await accountCollection.insertOne({
-                name: 'Andrei',
-                email: 'andrei.santos@test.com',
-                password: '123',
-                role: 'admin',
-            });
-
-            const id = res.ops[0]._id;
-            const accessToken = sign({ id }, env.jwtSecret);
-            await accountCollection.updateOne(
-                {
-                    _id: id,
-                },
-                {
-                    $set: { accessToken },
-                },
-            );
+            const accessToken = await makeAccessToken();
 
             await request(app)
                 .post('/api/survey')
@@ -86,22 +92,7 @@ describe('Main :: Routes :: SurveyRoutes', () => {
         });
 
         test('Should return 200 on load survey with valid accessToken', async () => {
-            const res = await accountCollection.insertOne({
-                name: 'Andrei',
-                email: 'andrei.santos@test.com',
-                password: '123',
-            });
-
-            const id = res.ops[0]._id;
-            const accessToken = sign({ id }, env.jwtSecret);
-            await accountCollection.updateOne(
-                {
-                    _id: id,
-                },
-                {
-                    $set: { accessToken },
-                },
-            );
+            const accessToken = await makeAccessToken();
 
             await surveyCollection.insertMany([
                 {
