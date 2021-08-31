223b6b15c2c0f6c7f41e995ccb60aaf6a36e68eb
Andrei Santos <andrei.as3@hotmail.com>
2021-08-31 00:02:53 -0300

    fix: revret update libs

--- src/main/config/Routes.ts
+++ src/main/config/Routes.ts
@@ -1,5 +1,5 @@
 import { Express, Router } from 'express';
-// import fg from 'fast-glob';
+// import fg from 'fast-glob';''
 import { readdirSync } from 'fs';
 
 export default (app: Express): void => {
@@ -7,7 +7,7 @@ export default (app: Express): void => {
     app.use('/api', router);
     // fg.sync('**/src/main/routes/**Routes.ts').map(async file => (await import(`../../../${file}`)).default(router));
     readdirSync(`${__dirname}/../routes`).map(async file => {
-        if (!file.includes('.test.')) {
+        if (!file.endsWith('.map')) {
             (await import(`../routes/${file}`)).default(router);
         }
     });
