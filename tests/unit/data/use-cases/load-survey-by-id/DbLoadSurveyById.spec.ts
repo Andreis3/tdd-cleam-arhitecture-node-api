4de1c63788b1642d4dc4f32096380b89359af95b
Andrei Santos <andrei.as3@hotmail.com>
2021-09-03 07:15:59 -0300

    test: ensure DbLoadSurveyById returns a Survy if  ILoadSurveyByIdRepository with correctsucceds

--- tests/unit/data/use-cases/load-survey-by-id/DbLoadSurveyById.spec.ts
+++ tests/unit/data/use-cases/load-survey-by-id/DbLoadSurveyById.spec.ts
@@ -57,4 +57,10 @@ describe('DbLoadById', () => {
 
         expect(loadByIdSpy).toHaveBeenCalledWith('any_id');
     });
+
+    test('Should return Survey on success', async () => {
+        const { sut } = makeSut();
+        const survey = await sut.loadById('any_id');
+        expect(survey).toEqual(makeFakeSurvey());
+    });
 });
