f7da84a3daa1273fd9b89ec60bf775b5bfbcdeb9
Andrei Santos <andrei.as3@hotmail.com>
2021-09-05 21:40:30 -0300

    feat: create SurveyResultMongoRepository save or upadet surveys results

---
+++ src/infra/db/mongodb/survey-result/SurveyResultMongoRepository.ts
@@ -0,0 +1,29 @@
+import {
+    ISaveSurveyResultRepository,
+    ISurveyResultModel,
+} from '@/data/use-cases/save-survey-result/DbSaveSurveyResultProtocols';
+import { ISaveSurveyResultModel } from '@/domain/use-cases/ISaveSurveyResult';
+import { MongoHelper } from '@/infra/db/mongodb/helpers/MongoHelpers';
+
+export class SurveyResultMongoRepository implements ISaveSurveyResultRepository {
+    async save(data: ISaveSurveyResultModel): Promise<ISurveyResultModel> {
+        const surveyResultCollection = await MongoHelper.getCollection('surveyResult');
+        const surveyResult = await surveyResultCollection.findOneAndUpdate(
+            {
+                surveyId: data.surveyId,
+                accountId: data.accountId,
+            },
+            {
+                $set: {
+                    answer: data.answer,
+                    data: data.date,
+                },
+            },
+            {
+                upsert: true,
+                returnOriginal: false,
+            },
+        );
+        return surveyResult.value && MongoHelper.map(surveyResult.value);
+    }
+}
